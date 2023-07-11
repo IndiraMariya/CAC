1b869910b3ee0760de6835f62965f2b2e926074c
Sabrina <59457991+syenko@users.noreply.github.com>
2023-07-11 14:46:57 -0700

    Extract content into Article and Topic components

---
+++ web-app/src/lib/components/Article.svelte
@@ -0,0 +1,37 @@
+<script>
+	import { slide } from 'svelte/transition';
+
+	export let title = '';
+	export let href = '';
+	export let description = '';
+	export let author = '';
+	export let date = '';
+	export let src = '';
+	export let alt = '';
+	export let newsSource = '';
+
+	let hovering = false;
+</script>
+
+<div class="relative shadow h-60 w-96 flex flex-row rounded-md overflow-hidden">
+	<a
+		{href}
+		on:mouseenter={() => {
+			hovering = true;
+		}}
+		on:mouseleave={() => {
+			hovering = false;
+		}}
+		class="w-full h-full"
+	>
+		<img {src} {alt} class="w-full h-full object-cover" />
+		<div class="absolute w-full text-white bg-black/30 bottom-0 p-3">
+			<div class="uppercase text-sm">{newsSource} - {new Date(date).toLocaleDateString()}</div>
+			<div class="font-bold text-xl">{title}</div>
+			<div>{author}</div>
+			{#if hovering}
+				<div class="text-sm mt-3" transition:slide>{description}</div>
+			{/if}
+		</div>
+	</a>
+</div>
---
+++ web-app/src/lib/components/Topic.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Article from './Article.svelte';
+
+	export let id = '';
+	export let name = '';
+	/**
+	 * @type {any[]}
+	 */
+	export let articleData = [];
+</script>
+
+<div class="mb-10 p-5 shadow shadow-black/50 rounded-lg">
+	<h1 class="capitalize font-bold mb-3 text-lg">{name}</h1>
+	<div class="flex flex-wrap justify-center gap-6">
+		{#each articleData as article}
+			<Article {...article} />
+		{/each}
+	</div>
+</div>
