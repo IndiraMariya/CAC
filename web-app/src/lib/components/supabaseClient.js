ad72e37fb79558ca63fc2d5864408eb6064ccd74
Sabrina <sabrinayenko@gmail.com>
2023-07-12 22:02:05 -0700

    Update supabase client

--- web-app/src/lib/components/supabaseClient.js
+++ web-app/src/lib/components/supabaseClient.js
@@ -1,3 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
+import { SUPABASE_URL, SUPABASE_KEY } from '$env/static/private';
 
-export const supabase = createClient('https://rehftaccvqxocrqyrfam.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJlaGZ0YWNjdnF4b2NycXlyZmFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODkwMzE2MjAsImV4cCI6MjAwNDYwNzYyMH0.-dH8LXuy2nWKq8cnTM91pYsJoIQlDgCKXYsHd-yx-jU')
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
