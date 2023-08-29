d5751607337005936c20ad308660514853b7e7f0
Sabrina <59457991+syenko@users.noreply.github.com>
2023-08-28 21:26:42 -0700

    Add bag-of-words initial project setup

---
+++ bag-of-words/main.py
@@ -0,0 +1,6 @@
+# Based on: https://scikit-learn.org/stable/tutorial/text_analytics/working_with_text_data.html
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+count_vect = CountVectorizer()
+
