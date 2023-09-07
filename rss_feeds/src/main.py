261c98c0dcfb67c2c5405e7b63b88e4e77e97597
Indira <indira.mariya@gmail.com>
2023-09-06 20:55:42 -0700

    create files for each news source

--- rss_feeds/src/main.py
+++ rss_feeds/src/main.py
@@ -1,29 +1,44 @@
-import washpost
-from . import os
+# from washpost import washpost
+# import os
+# from dotenv import load_dotenv
+# from supabase import Client
+# import csv
+# # import clean_data
+# from washpost import ReadRss
+from washpost import ReadRss
+import os
+from dotenv import load_dotenv
 from supabase import Client
+import csv
 
-def add_data(article_data):
-    global count
-    url = os.environ.get('SUPABASE_URL')
-    api_key = os.environ.get('SUPABASE_KEY')
-    # Initialize the Supabase client
-    supabase = Client(url, api_key)
 
-    name = article_data['title']
-    newsSource = article_data['newsSource']
-    #insert data
-    data = supabase.table("Data").upsert({
-        "name":name, 
-        "articleData": article_data, 
-        "newsSource": newsSource}).execute()
-    assert len(data.data) > 0
-    if data.data and data.data[0].get('status') == 'ok':
-        count = count + 1
+headers = {
+    'User-Agent': 'your-user-agent-here'
+}
+
+
 
 if __name__ == '__main__':
+    def switch_source(case):
+        if case == 'Washington Post':
+            ReadRss(row[0], headers, row[1])
+            print(f"{case} successfully added" )
+        elif case == 'New York Times':
+            print("This is case 2")
+        elif case == 'Fox News':
+            print("This is case 3")
+        elif case == 'Wall Street Journal':
+            print("This is case 4")
+        else:
+            print("This is the default case")
+
     path = os.environ.get('SOURCE_PATH')
-    with open (path) as file:
+    
+    with open ('/Users/indiram/Work/CAC/rss_feeds/rss_feeds.csv') as file:
         content = csv.reader(file)
         for row in content:
-            feed = washpost.ReadRss(row[0], headers, row[1])
-    print(f"{count} Articles Added")
+            source = row[1]
+            switch_source(source)
+    
+    # print(f"{count} Articles Added")
+    # clean_data.clearArticles()
