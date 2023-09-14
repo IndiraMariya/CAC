5a778234ed6304cef47ef156f98b56af30e93921
Indira <indira.mariya@gmail.com>
2023-09-13 23:43:42 -0700

    added footer

---
+++ web-app/src/lib/components/footer.svelte
@@ -0,0 +1,10 @@
+<div class="footer">
+    <p>&copy; 2023 THE DOUBLE SCOOP CO. All rights reserved.</p>
+    <p>123 Main Street, Los Altos, US</p>
+    <p>Email: info@yourcompany.com | Phone: +1 (123) 456-7890</p>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About Us</a></li>
+        <li><a href="/contact">Contact</a></li>
+    </ul>
+</div>
