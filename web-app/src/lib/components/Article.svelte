518a4a92c1c4513cb64542939db74b9808737043
Indira <indira.mariya@gmail.com>
2023-09-13 23:45:29 -0700

    added nav and reformatted articles

--- web-app/src/lib/components/Article.svelte
+++ web-app/src/lib/components/Article.svelte
@@ -13,7 +13,41 @@
 	let hovering = false;
 </script>
 
-<div class="relative shadow h-60 w-96 flex flex-row rounded-md overflow-hidden">
+<div class="relative group shadow h-70 w-96 flex flex-col rounded-md overflow-hidden">
+    <div class="flex justify-between">
+        <div class=" font-bold p-2" style="font-style:oblique; background-color: #FFFFFF; border-radius: 10px;">{newsSource}</div>
+        <div class="p-2 text-right" style="color:red;background-color: #FFFFFF; border-radius: 10px;">{new Date(date).toLocaleDateString()}</div>
+    </div>
+
+    <div class="bg-gray-200 p-2">
+        <div class="font-bold text-xl line-clamp-2 overflow-hidden">{title}</div>
+    </div>
+
+    <a
+        {href}
+        on:mouseenter={() => {
+            hovering = true;
+        }}
+        on:mouseleave={() => {
+            hovering = false;
+        }}
+        class="w-full h-full"
+    >
+        <img {src} {alt} class="w-full h-40 object-cover" />
+        <div class="p-3 overflow-hidden">
+            <div>{author}</div>
+            {#if hovering}
+                <div class="text-sm mt-3" transition:slide>{description}</div>
+            {/if}
+        </div>
+    </a>
+</div>
+
+
+
+
+
+<!-- <div class="relative shadow h-60 w-96 flex flex-row rounded-md overflow-hidden transition-transform hover:scale-105 hover:border-2 border-transparent hover:border-red-500">
 	<a
 		{href}
 		on:mouseenter={() => {
@@ -25,7 +59,7 @@
 		class="w-full h-full"
 	>
 		<img {src} {alt} class="w-full h-full object-cover" />
-		<div class="absolute w-full text-white bg-black/30 bottom-0 p-3">
+		<div class="absolute w-full bg-black/30 bottom-0 p-3" style="color:rgb(155,73,155);">
 			<div class="uppercase text-sm">{newsSource} - {new Date(date).toLocaleDateString()}</div>
 			<div class="font-bold text-xl">{title}</div>
 			<div>{author}</div>
@@ -34,4 +68,6 @@
 			{/if}
 		</div>
 	</a>
-</div>
+</div> -->
+
+
--- web-app/src/lib/components/Nav.svelte
+++ web-app/src/lib/components/Nav.svelte
@@ -2,22 +2,119 @@
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Source+Serif+4:ital,opsz,wght@0,8..60,200;0,8..60,300;0,8..60,900;1,8..60,200;1,8..60,900&display=swap" rel="stylesheet">
 
-<div class="bg-blue-100 text-black fixed top-0 left-0 right-0 z-50 py-2 text-center">
+<div class=" text-black  top-0 left-0 right-0 z-50 py-2 text-center">
     <a href="/">
         <!-- svelte-ignore a11y-img-redundant-alt -->
         <img src="/src/static/cartoonsketches500.png" alt="Description of the image" width="300" height="200" class="mx-auto">
     </a>
 </div>
 
-<div class="bg-blue-100 text-black fixed top-40 left-0 right-0 z-50 text-center">
-    <!-- Add a <div> for padding -->
-    <div class="p-4">
-        <nav class="container mx-auto flex justify-evenly items-center py-2 border-t border-black border-t-2">
-            <a class="nav-link px-4" href="/">Home</a>
-            <a class="nav-link px-4" href="/explore">Explore</a>
-            <a class="nav-link px-4" href="/faq">FAQ</a>
-            <a class="nav-link px-4" href="/about">About Us</a>
-            <a class="nav-link px-4" href="/learn">Learn More</a>
+<div class=" text-black top-40 left-0 right-0 z-50 text-center" >
+    <div class="p-4" style="color: rgb(59,73,155; font-weight: bold; font-size: 25px;">
+        <nav class="container mx-auto flex justify-evenly items-center py-2 ">
+            <a class="nav-link px-4 hover:text-red-500" href="/"><i>HOME</i></a>
+            <a class="nav-link px-4 hover:text-red-500" href="/explore"><i>EXPLORE</i></a>
+            <a class="nav-link px-4 hover:text-red-500" href="/about"><i>ABOUT</i></a>
+            <a class="nav-link px-4 hover:text-red-500" href="/learn"><i>LEARN</i></a>
+            <a class="nav-link px-4 hover:text-red-500" href="/contact"><i>CONTACT</i></a>
         </nav>
     </div>
 </div>
+<!-- <script>
+    let isHovered = false;
+  
+    function toggleHover() {
+      isHovered = !isHovered;
+    }
+  
+    function handleBlur() {
+      isHovered = false;
+    }
+</script> -->
+  
+  <!-- <style>
+    /* Add your Tailwind CSS classes here */
+    .red-dot {
+      left: -4px; /* Adjust the left position as needed */
+      top: 50%; /* Adjust the top position as needed */
+      transform: translateY(-50%);
+      width: 8px;
+      height: 8px;
+      background-color: red;
+      border-radius: 50%;
+      position: absolute;
+    }
+  </style> -->
+  
+  <!-- <div class="text-black top-0 left-0 right-0 z-50 py-2 text-center">
+    <a href="/">
+      <img src="/src/static/cartoonsketches500.png" alt="Description of the image" width="300" height="200" class="mx-auto" />
+    </a>
+  </div> -->
+  
+  <!-- <div class="text-black top-40 left-0 right-0 z-50 text-center">
+    <div class="p-4" style="color: rgb(59,73,155); font-weight: bold; font-size: 25px;">
+      <nav class="container mx-auto flex justify-evenly items-center py-2">
+        <a
+          class="nav-link px-4 hover:text-red-500 relative"
+          href="/"
+          on:mouseover={toggleHover}
+          on:focus={toggleHover}
+          on:mouseout={handleBlur}
+          on:blur={handleBlur}
+          tabindex="0"
+        >
+          <i>HOME</i>
+          {#if isHovered}
+            <span class="red-dot"></span>
+          {/if}
+        </a>
+
+        <a
+        class="nav-link px-4 hover:text-red-500 relative"
+        href="/explore"
+        on:mouseover={toggleHover}
+        on:focus={toggleHover}
+        on:mouseout={handleBlur}
+        on:blur={handleBlur}
+        tabindex="0"
+      >
+        <i>EXPLORE</i>
+        {#if isHovered}
+          <span class="red-dot"></span>
+        {/if}
+      </a>
+
+      <a
+      class="nav-link px-4 hover:text-red-500 relative"
+      href="/"
+      on:mouseover={toggleHover}
+      on:focus={toggleHover}
+      on:mouseout={handleBlur}
+      on:blur={handleBlur}
+      tabindex="0"
+    >
+      <i>HOME</i>
+      {#if isHovered}
+        <span class="red-dot"></span>
+      {/if}
+    </a>
+
+    <a
+    class="nav-link px-4 hover:text-red-500 relative"
+    href="/"
+    on:mouseover={toggleHover}
+    on:focus={toggleHover}
+    on:mouseout={handleBlur}
+    on:blur={handleBlur}
+    tabindex="0"
+  >
+    <i>HOME</i>
+    {#if isHovered}
+      <span class="red-dot"></span>
+    {/if}
+  </a>
+      </nav>
+    </div>
+  </div>
+   -->
