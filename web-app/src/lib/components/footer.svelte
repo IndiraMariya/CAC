98722c9716b346995b455766c98befee4cbf9d73
Indira <indira.mariya@gmail.com>
2023-10-01 21:19:37 -0700

    updated footer added search icon

--- web-app/src/lib/components/footer.svelte
+++ web-app/src/lib/components/footer.svelte
@@ -1,10 +1,22 @@
+<style>
+    .footer{
+        font-family: 'Source Serif 4';
+        color: white;
+        text-align: center;
+        padding-top: 10px;
+        padding-bottom: 10px;
+        border-top: 1px solid rgb(59,73,155);
+        background-color: rgb(59,73,155);
+    }
+</style>
+
 <div class="footer">
     <p>&copy; 2023 THE DOUBLE SCOOP CO. All rights reserved.</p>
     <p>123 Main Street, Los Altos, US</p>
     <p>Email: info@yourcompany.com | Phone: +1 (123) 456-7890</p>
-    <ul>
+    <!-- <ul>
         <li><a href="/">Home</a></li>
         <li><a href="/about">About Us</a></li>
         <li><a href="/contact">Contact</a></li>
-    </ul>
+    </ul> -->
 </div>
--- web-app/src/routes/+page.svelte
+++ web-app/src/routes/+page.svelte
@@ -2,36 +2,64 @@
 	import Article from '$lib/components/Article.svelte';
 	import Nav from '$lib/components/Nav.svelte';
 	import Footer from '$lib/components/footer.svelte';
+	import Modal from '$lib/components/Modal.svelte';
+	import Search from '$lib/components/Search.svelte'; 
 	export let data;
+	let showModal = false;
 </script>
 
 
 
 <style>
+    @import url('https://fonts.googleapis.com/css2?family=Source+Serif+4:opsz,wght@8..60,200&display=swap');
 	main {
-	  padding-top: 10px; /* Adjust the value to match the height of your navbar */
-	  /* font-family: 'Source Serif 4', sans-serif; */
-	  font-weight: 200;
+	  padding-top: 10px; 
+	  font-family: 'Source Serif 4';
 	  background-color: rgba(59, 73, 155, 0.126);
-	  border-radius: 15px;
+	  border: 1px solid rgb(34, 44, 98);
+
+	  /* border-radius: 15px; */
 	}
 	.page-container {
-        border: 10px solid rgb(59,73,155); /* Adjust the border width and color as needed */
-        padding: 20px; /* Add padding to create space between the content and the border */
-		color: rgb(59,73,155);
-		
+        /* border: 30px solid rgb(59,73,155);  */
+        padding: 20px; 
+		color: rgb(34, 44, 98);
+		zoom: 0.8; 
+        width: 100%;
     }
   </style>
+<Search></Search>
+<Modal bind:showModal>
+	<h2 class="text-3xl">
+		Article Information
+	</h2>
+
+	<p>
+		Title: Lorem Ipsum Article
+		Author: John Doe
+		Publication Date: October 1, 2023
+
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue. Vestibulum id ligula porta felis euismod semper. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+
+		Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Vestibulum id ligula porta felis euismod semper. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Nulla vitae elit libero, a pharetra augue. Etiam porta sem malesuada magna mollis euismod.
+
+		Cras mattis consectetur purus sit amet fermentum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nullam quis risus eget urna mollis ornare vel eu leo. Donec id elit non mi porta gravida at eget metus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nulla vitae elit libero, a pharetra augue.
+
+		Sed posuere consectetur est at lobortis. Nulla vitae elit libero, a pharetra augue. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Aenean lacinia bibendum nulla sed consectetur. Vestibulum id ligula porta felis euismod semper. Donec id elit non mi porta gravida at eget metus.
+	</p>
+
+	<a href="https://www.merriam-webster.com/dictionary/modal">merriam-webster.com</a>
+</Modal>
 
 <div class="page-container">
 	<Nav></Nav>
 	<main>
 		<div class="p-6">
-			<h1 class="pb-5 text-4xl font-bold">Your Daily Scoop</h1>
-			<div class="flex flex-wrap gap-4">
+			<h1 class="font-normal pb-5 text-4xl font-bold">Your Daily Scoop</h1>
+			<div class="articles flex flex-wrap gap-4 ">
 				{#each data.articles as article}
 					{#if article.articleData}
-						<Article {...article.articleData} />
+						<Article {...article.articleData} bind:showModal={showModal}/>
 					{/if}
 				{/each}
 			</div>
@@ -41,4 +69,4 @@
 
 <Footer></Footer>
 
-
+<!-- <Topic></Topic> -->
