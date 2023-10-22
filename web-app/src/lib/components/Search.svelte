93c459976d54e8f299d2a3557fa290e79fa1fc45
Indira <indira.mariya@gmail.com>
2023-10-22 00:00:57 -0700

    add search box

--- web-app/src/lib/components/Search.svelte
+++ web-app/src/lib/components/Search.svelte
@@ -1,24 +1,21 @@
 <script>
-    function shift() {
-        const icon = document.getElementById("searchIcon");
-        if (icon != null)
-            icon.classList.add("shift");
-    }
+    let showMessage = false;
+    let isClicked = false;
+
+    let searchText = "";
+  
+    function toggleMessage() {
+      showMessage = !showMessage;
+      isClicked = !isClicked;
+  }
+
 </script>
-
-<button on:click={shift}>
-    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" class="fill-red-300 hover:fill-red-500" id = "searchIcon"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-
-        <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/> 
-    </svg>
-</button>
-
-<button on:click={shift}>
-    click me
-</button>
-
-
-<style>
+  
+  <style>
+    .move-right {
+      transform: translateX(-200px); /* Adjust the value to control the shift distance */
+      transition: transform 0.3s; /* Add a smooth transition effect */
+    }
     svg{
         position:absolute;
         padding:20px;
@@ -26,7 +23,31 @@
         width:70px;
         height:70px;
     }
-    .shift{
-        transform: translateX(0px);
+    input{
+        position:absolute;
+        padding:20px;
+        right:10px;
+        width:200px;
+        height:50px;
+        border-radius: 5px;
     }
-</style>
+    div{
+        background-color: #FDF5F0;
+    }
+  </style>
+  
+  <div>
+    <button on:click={toggleMessage}>
+        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" class="fill-red-300 hover:fill-red-500" id = "searchIcon" class:move-right={isClicked}><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+            <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/> 
+        </svg>
+      </button>
+  </div>
+
+  
+  {#if showMessage}
+    <input type="text" placeholder="Search.." bind:value={searchText}>
+    <p>You entered: {searchText}</p> <!-- Display the entered text -->
+  {/if}
+
+ 
