fc1b190a54948554ecd51bde288664f32fb74919
Sabrina <59457991+syenko@users.noreply.github.com>
2023-10-22 20:02:06 -0700

    Add search term from client-side input to server side query

--- web-app/src/routes/+page.server.js
+++ web-app/src/routes/+page.server.js
@@ -1,16 +1,16 @@
 import { supabase } from '$lib/components/supabaseClient.js';
-// import search_term from '$lib/components/Search.svelte';
-let search_term = ""
 
-export async function load() {
+export async function load({ url }) {
+	const search_term = url.searchParams.get('q');
+
 	const { data, error } = await supabase
 		.rpc('get_data')
 		.ilike('name', `%${search_term}%`)
 		// .order('topic', { ascending: false, nullsFirst: false })
 		.order('date', { ascending: false, nullsFirst: false })
-		.limit(150)
-	
-		return {
+		.limit(150);
+
+	return {
 		articles: data ?? []
-		};
+	};
 }
