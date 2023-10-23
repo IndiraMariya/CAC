30e8a903e1fb406a0113c536dd2345b725e78c8a
Sabrina <59457991+syenko@users.noreply.github.com>
2023-10-22 20:27:18 -0700

    Fix footer to always be at bottom

--- web-app/src/lib/components/footer.svelte
+++ web-app/src/lib/components/footer.svelte
@@ -1,8 +1,8 @@
-<div class="py-3 text-white bg-p_blue font-body text-center">
-    <p>&copy; 2023 THE DOUBLE SCOOP CO. All rights reserved.</p>
-    <p>123 Main Street, Los Altos, US</p>
-    <p>Email: info@yourcompany.com | Phone: +1 (123) 456-7890</p>
-    <!-- <ul>
+<div class="py-3 text-white bg-p_blue font-body text-center justify-self-end">
+	<p>&copy; 2023 THE DOUBLE SCOOP CO. All rights reserved.</p>
+	<p>123 Main Street, Los Altos, US</p>
+	<p>Email: info@yourcompany.com | Phone: +1 (123) 456-7890</p>
+	<!-- <ul>
         <li><a href="/">Home</a></li>
         <li><a href="/about">About Us</a></li>
         <li><a href="/contact">Contact</a></li>
--- web-app/src/routes/+page.svelte
+++ web-app/src/routes/+page.svelte
@@ -5,7 +5,7 @@
 	import Modal from '$lib/components/Modal.svelte';
 	import Search from '$lib/components/Search.svelte';
 	import search_term from '$lib/components/Search.svelte';
-	
+
 	export let data;
 	let showModal = false;
 	let modalData = {
@@ -21,29 +21,33 @@
 	};
 </script>
 
-<Search />
-<Modal bind:showModal bind:data={modalData} />
+<div class="min-h-[100vh] flex flex-col justify-between">
+	<div>
+		<Search />
+		<Modal bind:showModal bind:data={modalData} />
 
-<div class="p-5 text-black w-100 bg-p_bg">
-	<Nav />
-	<div class="font-body border-[1px] border-black">
-		<div class="px-6">
-			<h1 class="font-light font-display italic py-4 text-2xl color-p_text">
-				What's the daily scoop?
-			</h1>
-			<div class="articles grid grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 gap-4">
-				<!-- TODO: Account for accurately grouping articles without images -->
-				{#each data.articles as article}
-					{#if article.articleData}
-						<Article {...article.articleData} bind:showModal bind:modalData />
-					{/if}
-				{/each}
+		<div class="p-5 text-black w-full h-full">
+			<Nav />
+			<div class="font-body border-[1px] border-black">
+				<div class="px-6">
+					<h1 class="font-light font-display italic py-4 text-2xl color-p_text">
+						What's the daily scoop?
+					</h1>
+					<div class="articles grid grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 gap-4">
+						<!-- TODO: Account for accurately grouping articles without images -->
+						{#each data.articles as article}
+							{#if article.articleData}
+								<Article {...article.articleData} bind:showModal bind:modalData />
+							{/if}
+						{/each}
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
+
+	<div class="justify-self-end">
+		<Footer />
+	</div>
 </div>
-
-<Footer />
-
-
 <!-- <Topic></Topic> -->
