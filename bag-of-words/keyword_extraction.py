5bf8a9b57e1a88cf8bd513f701f2513adfb3d1bb
Sabrina <59457991+syenko@users.noreply.github.com>
2023-10-24 09:55:30 -0700

    Reformat functions for better modularity

--- bag-of-words/keyword_extraction.py
+++ bag-of-words/keyword_extraction.py
@@ -16,34 +16,47 @@ list_data = grouped_data["text"].values.tolist()
 test_data = list_data[0]
 
 
-# =============================== Create Spacy Doc
-def extract_texacy_keyterms():
-    doc = textacy.make_spacy_doc(test_data, lang="en_core_web_sm")
+# =============================== Key Term Functions
+def extract_texacy_keyterms(data):
+    doc = textacy.make_spacy_doc(data, lang="en_core_web_sm")
 
     POS = ['PROPN', 'NOUN', 'ADJ', 'VERB', 'ADV']
     TOP_N = 10
     NORMALIZER = textacy.spacier.utils.get_normalized_text
 
-    print(extract.keyterms.textrank(doc, normalize=NORMALIZER, include_pos=POS, topn=TOP_N))
-    print(extract.keyterms.yake(doc, normalize='lemma', include_pos=POS, topn=TOP_N))
-    print(extract.keyterms.scake(doc, normalize=NORMALIZER, include_pos=POS, topn=TOP_N))
-    print(extract.keyterms.sgrank(doc, ngrams=(1, 2, 3), normalize=NORMALIZER, include_pos=POS, topn=TOP_N))
+    textrank = extract.keyterms.textrank(doc, normalize=NORMALIZER, include_pos=POS, topn=TOP_N)
+    yake = extract.keyterms.yake(doc, normalize='lemma', include_pos=POS, topn=TOP_N)
+    scake = extract.keyterms.scake(doc, normalize=NORMALIZER, include_pos=POS, topn=TOP_N)
+    sgrank = extract.keyterms.sgrank(doc, ngrams=(1, 2, 3), normalize=NORMALIZER, include_pos=POS, topn=TOP_N)
+
+    print(textrank)
+    print(yake)
+    print(scake)
+    print(sgrank)
+
+    return textrank, yake, scake, sgrank
 
 
-def extract_keybert_keyterms(kw_model):
-    keywords = kw_model.extract_keywords(test_data, keyphrase_ngram_range=(1, 3), stop_words="english")
-    print(keywords)
+def extract_keybert_keyterms(data, kw_model, vectorizer=None, debug=False):
+    if not vectorizer:
+        keywords = kw_model.extract_keywords(data, keyphrase_ngram_range=(1, 3), stop_words="english")
+    else:
+        keywords = kw_model.extract_keywords(data, vectorizer=vectorizer)
+    if debug: print(keywords)
+    return keywords
 
 # Create KeyBERT
 kw_model = KeyBERT()
 nlp = spacy.load("en_core_web_lg", exclude=['tagger', 'parser', 'ner', 'attribute_ruler', 'lemmatizer'])
 kw_spacy_model = KeyBERT(model=nlp)
+vectorizer = KeyphraseCountVectorizer()
 
 # print("---- TEXACY KEYTERMS ----")
 # extract_texacy_keyterms()
 
+test_data = list_data[:10]
 print("---- KeyBERT KEYTERMS ----")
-extract_keybert_keyterms(kw_model)
-extract_keybert_keyterms(kw_spacy_model)
-print(kw_model.extract_keywords(test_data, vectorizer=KeyphraseCountVectorizer()))
-print(kw_spacy_model.extract_keywords(test_data, vectorizer=KeyphraseCountVectorizer()))
+extract_keybert_keyterms(test_data, kw_model)
+extract_keybert_keyterms(test_data, kw_spacy_model)
+extract_keybert_keyterms(test_data, kw_model, vectorizer=vectorizer) # best atm
+extract_keybert_keyterms(test_data, kw_spacy_model, vectorizer=vectorizer)
