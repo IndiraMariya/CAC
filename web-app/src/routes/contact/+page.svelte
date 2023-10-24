1746a00dc04227657fa9d3194edb81120aa4b5d3
Indira <indira.mariya@gmail.com>
2023-10-23 20:42:55 -0700

    updated contact page

--- web-app/src/routes/contact/+page.svelte
+++ web-app/src/routes/contact/+page.svelte
@@ -4,30 +4,38 @@
 	import PageContainer from '$lib/components/PageContainer.svelte';
     // Data for each member
     let members = [
-      {
-        name: 'Indira Mariya',
-        role: 'Developer',
-        blurb: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non purus at ante aliquet.',
-        image: '/filler_img.jpeg', // Image file path
-      },
-      {
-        name: 'Jane Smith',
-        role: 'Developer',
-        blurb: 'Vestibulum cursus ultricies nulla, ut vehicula ligula iaculis at. Fusce tristique malesuada.',
-        image: '/filler_img.jpeg',
-      },
-      {
-        name: 'Bob Johnson',
-        role: 'Designer',
-        blurb: 'Aenean a libero elit. Phasellus egestas vel elit id bibendum. Vivamus bibendum euismod dui.',
-        image: '/filler_img.jpeg',
-      },
-      {
-        name: 'Shmob Johnson',
-        role: 'Designer',
-        blurb: 'Aenean a libero elit. Phasellus egestas vel elit id bibendum. Vivamus bibendum euismod dui.',
-        image: '/filler_img.jpeg',
-      },
+    {
+      name: 'Indira Mariya',
+      role: 'Developer',
+      blurb: 'I like coding. My favorite flavor of ice cream is vanilla.',
+      image: '/CACindira.png',
+      border: 'rgb(190, 137, 250)',
+      background: 'rgba(190, 137, 250, 0.2)'
+    },
+    {
+      name: 'Sabrina Yen-Ko',
+      role: 'Developer',
+      blurb: 'Vestibulum cursus ultricies nulla, ut vehicula ligula iaculis at. Fusce tristique malesuada.',
+      image: '/CACsabrina.png',
+      border: '#4fb880',
+      background: 'rgba(79, 184, 128, 0.1)'
+    },
+    {
+      name: 'Leo Troin',
+      role: 'Designer',
+      blurb: 'Aenean a libero elit. Phasellus egestas vel elit id bibendum. Vivamus bibendum euismod dui.',
+      image: '/CACleo.png',
+      border: 'rgb(244, 175, 98)',
+      background: 'rgba(244, 175, 98, 0.3)'
+    },
+    {
+      name: 'Kaitlyn Don',
+      role: 'Designer',
+      blurb: 'Aenean a libero elit. Phasellus egestas vel elit id bibendum. Vivamus bibendum euismod dui.',
+      image: '/CACkaitlyn.png',
+      border: 'rgb(255, 174, 207)',
+      background: 'rgba(255, 174, 207, 0.2)'
+    },
     ];
 </script>
 
@@ -35,19 +43,18 @@
     <header>
         <Nav></Nav>
     </header>
-    <h1 class="pb-5 text-4xl ">Meet the Members</h1>
+    <h1 class="pb-5 text-4xl ">Meet the Team</h1>
   
-  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
-    {#each members as member (member.name)}
-      <div class="bg-white shadow-md p-4 rounded-lg">
-        <img src={member.image} alt={member.name} class="w-full h-auto" />
-  
-        <h2 class="text-xl font-semibold mt-2">{member.name}</h2>
-        <p class="text-gray-600">{member.role}</p>
-        <p class="text-sm mt-2">{member.blurb}</p>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
+        {#each members as member (member.name)}
+          <div class="bg-white shadow-md p-4 rounded-lg" style="border: {member.border} solid 2px; background-color: {member.background};">
+            <img src={member.image} alt={member.name} class="w-full h-auto" />
+            <h2 class="text-xl font-semibold mt-2">{member.name}</h2>
+            <p class="text-gray-600">{member.role}</p>
+            <p class="text-sm mt-2">{member.blurb}</p>
+          </div>
+        {/each}
       </div>
-    {/each}
-  </div>  
     
 </PageContainer>
 
